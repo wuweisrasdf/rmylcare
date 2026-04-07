c9cf8707a8e9b68431b0022a30fa9ba83d0cac6b
wuweisrasdf <srasdf7@163.com>
2026-04-07 09:23:48 +0800

    发送微信小程序时用的对象

--- healthcare-common/src/main/java/com/healthcare/common/utils/SysWxMessage.java
+++ healthcare-common/src/main/java/com/healthcare/common/utils/SysWxMessage.java
@@ -2,6 +2,8 @@ package com.healthcare.common.utils;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import com.healthcare.common.config.RuoYiConfig;
 /*
  * 小程序推送所需数据
  * */
@@ -9,6 +11,7 @@ public class SysWxMessage {
     private String touser;//用户openid
     private String template_id;//订阅消息模版id
     private String page = "pages/index/index";//默认跳到小程序首页
+    private String miniprogram_state;
     private Map<String, SysWxTempData> data=new HashMap<String,SysWxTempData>();//推送文字
 
     public String getTouser() {
@@ -35,4 +38,17 @@ public class SysWxMessage {
     public void setData(Map<String, SysWxTempData> data) {
         this.data = data;
     }
+	public String getMiniprogram_state() {
+		return miniprogram_state;
+	}
+	public void setMiniprogram_state(String miniprogram_state) {
+		String version = RuoYiConfig.getVersion();
+		if(!version.equals("3.8.7")) {
+			this.miniprogram_state = "trial";
+		}
+		else {
+			this.miniprogram_state = "formal";
+		}
+	}
+    
 }
